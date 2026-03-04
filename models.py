7c8d5dd28dce1ce868c1a4e6e4e48cb71818286c
dtaimur <dtaimur@terpmail.umd.edu>
2026-03-03 19:45:43 -0500

    adding login credentials

--- models.py
+++ models.py
@@ -1,24 +1,24 @@
 from sqlalchemy import Column, Integer, String, Float, ARRAY, JSON
 from database import Base
+from sqlalchhemy.orm import relationship
 
 
 class UserProfile(Base):
     __tablename__ = "user_profiles"
 
-    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, primary_key=True, index=True)
     age = Column(Integer, nullable=False)
     height_feet = Column(Integer, nullable=False)
     height_inches = Column(Integer, nullable=False)
     weight = Column(Float, nullable=False)
     gender = Column(String, nullable=False)
-
     goal = Column(String, nullable=False)
     dietary_preferences = Column(ARRAY(String), default=[])
     allergies = Column(ARRAY(String), default=[])
     medical_conditions = Column(ARRAY(String), default=[])
-
     budget_level = Column(String)
     cooking_time = Column(String)
+    recipes = relationship("Recipe", back_populates="user")
 
 
 class Recipe(Base):
@@ -39,3 +39,4 @@ class Recipe(Base):
     timing = Column(JSON)
     dietary_tags = Column(JSON)
     estimated_cost = Column(Float)
+    user = relationship("UserProfile", back_populates="recipes")
